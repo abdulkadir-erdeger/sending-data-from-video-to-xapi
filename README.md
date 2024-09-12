c87f8ef1ce366df8f03dd7a6a956e16fa4e59d9c
Abdulkadir Erdeğer <abdulkadirerdeger06@gmail.com>
2024-09-12 09:12:24 +0300

    create project

---
+++ README.md
@@ -0,0 +1,24 @@
+# Video SCORM Render Projesi
+
+Bu proje, kullanıcıların video dosyalarını seçip SCORM formatında bir zip dosyası olarak dışa aktarmalarına olanak tanır.
+
+## Dosya Yapısı
+
+- `index.html`: HTML yapısı ve video dosyası seçimi ile render butonu.
+- `README.md`: Proje açıklaması ve kullanım talimatları.
+
+## Kullanım
+
+1. Tarayıcınızda `index.html` dosyasını açın.
+2. Video dosyasını yükleyin ve "Scorm'a Çevir" butonuna tıklayın.
+3. Oluşan SCORM zip dosyası otomatik olarak indirilecektir.
+
+## Geliştirici Notları
+
+- `JSZip` kütüphanesi SCORM zip dosyasını oluşturmak için kullanılır.
+- SCORM manifest dosyasının yapısını gerektiği şekilde özelleştirin.
+- Projeyi geliştirmek için ek dosya ve yapılandırmaları dahil edebilirsiniz.
+
+## Lisans
+
+Bu proje [MIT Lisansı](https://opensource.org/licenses/MIT) altında lisanslanmıştır.
---
+++ xapi.js
@@ -0,0 +1,78 @@
+
+// URL parametrelerini almak için
+let queryParams = new URLSearchParams(window.location.search);
+let lrsEndpoint = queryParams.get('endpoint');
+let auth = queryParams.get('auth');
+let actor = JSON.parse(queryParams.get('actor'));
+
+function sendXAPIData(verb, statement) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", lrsEndpoint + "statements", true);
+    xhr.setRequestHeader("Authorization", auth);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-Experience-API-Version", "1.0.3");
+
+    let xAPIData = {
+        actor: {
+            name: actor.name[0],
+            mbox: actor.mbox[0]
+        },
+        verb: {
+            id: `http://adlnet.gov/expapi/verbs/${verb}`,
+            display: { "en-US": verb }
+        },
+        object: {
+            id: document.location.href,
+            definition: {
+                name: { "en-US": "Video" },
+                description: { "en-US": statement }
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(xAPIData));
+}
+
+function getLastVideoTime() {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+
+        // Video'nun tanımlayıcı URL'si
+        let videoUrl = encodeURIComponent(window.location);
+
+        // Aktör bilgilerini JSON formatında oluştur ve encode et
+        let actorMail = JSON.stringify({
+            mbox: `${actor.mbox[0]}`
+        });
+
+        // XAPI endpoint URL'si
+        let url = `${lrsEndpoint}statements?agent=${actorMail}&activity=${videoUrl}`;
+
+        xhr.open("GET", url, true);
+        xhr.setRequestHeader("Authorization", auth);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-Experience-API-Version", "1.0.3");
+
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                let response = JSON.parse(xhr.responseText);
+                if (response.statements && response.statements.length > 0) {
+                    let lastStatement = response.statements[0];
+                    if (lastStatement.verb.id === "http://adlnet.gov/expapi/verbs/paused") {
+                        let match = lastStatement.object.definition.description['en-US'].match(/Video (\d+) saniyede durdu./);
+                        resolve(match[1]);
+
+                    }
+                    else {
+                        resolve(0);
+
+                    }
+                }
+            }
+            resolve(0);
+
+        };
+
+        xhr.send();
+    });
+}
